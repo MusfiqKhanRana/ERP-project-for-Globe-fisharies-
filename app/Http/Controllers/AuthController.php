04b6fa8e0f489fe4fa59b46a8cc5a79f868e9a86
Globetech-GTCL <glebetechcompanylimited@gmail.com>
2021-11-25 10:46:38 +0600

    dashboard path edit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
             // return Auth::user();
-            return redirect()->intended('dashboard')
+            return redirect()->intended('admin/dashboard')
                         ->withSuccess('Signed in');
         }
   
