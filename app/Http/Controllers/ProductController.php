279766f1cd72ee46519c38b651560965544de784
MusfiqKhanRana <12345rana.rk@gmail.com>
2021-12-05 14:07:38 +0600

    ajax DropDown

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Cutomer;
+use App\Models\Pack;
 use App\Models\Product;
 use App\Models\StockProduct;
 use App\Models\Warehouse;
 use Illuminate\Foundation\Console\Presets\React;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 class ProductController extends Controller
 {
     public function productIndex()
@@ -130,5 +132,70 @@ class ProductController extends Controller
         $stock_product = StockProduct::where('warehouse_id', $id)
             ->distinct()->get(['product_id']);
         return view('backend.product.warehouse_stock_product', compact('stock_product','id','warehouse'));
+    }
+    public function packsize()
+    {
+        $data = Pack::all();
+        return response()->json(['data' => $data]);
+    }
+    function action(Request $request)
+    {
+     if($request->ajax())
+     {
+      $output = '';
+      $query = $request->get('query');
+      if($query != '')
+      {
+       $data = Product::where('category_id', 'like', '%'.$query.'%')
+         ->orWhere('product_name', 'like', '%'.$query.'%')
+         ->orWhere('product_id', 'like', '%'.$query.'%')
+         ->orWhere('unit', 'like', '%'.$query.'%')
+         ->orWhere('buying_price', 'like', '%'.$query.'%')
+         ->orWhere('selling_price', 'like', '%'.$query.'%')
+         ->orWhere('online_selling_price', 'like', '%'.$query.'%')
+         ->orWhere('inhouse_selling_price', 'like', '%'.$query.'%')
+         ->orWhere('retail_selling_price', 'like', '%'.$query.'%')
+         ->orWhere('pack_size', 'like', '%'.$query.'%')
+         ->orderBy('product_id', 'desc')
+         ->get();
+         
+      }
+      else
+      {
+       $data = DB::table('tbl_customer')
+       ->orderBy('product_id', 'desc')
+         ->get();
+      }
+      $total_row = $data->count();
+      if($total_row > 0)
+      {
+       foreach($data as $row)
+       {
+        $output .= '
+        <tr>
+         <td>'.$row->CustomerName.'</td>
+         <td>'.$row->Address.'</td>
+         <td>'.$row->City.'</td>
+         <td>'.$row->PostalCode.'</td>
+         <td>'.$row->Country.'</td>
+        </tr>
+        ';
+       }
+      }
+      else
+      {
+       $output = '
+       <tr>
+        <td align="center" colspan="5">No Data Found</td>
+       </tr>
+       ';
+      }
+      $data = array(
+       'table_data'  => $output,
+       'total_data'  => $total_row
+      );
+
+      echo json_encode($data);
+     }
     }
 }
