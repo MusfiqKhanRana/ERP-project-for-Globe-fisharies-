b318b28ba142b85160405bf5249dc44d8028575c
MusfiqKhanRana <12345rana.rk@gmail.com>
2021-12-08 12:50:24 +0600

    product order module

---
+++ app/Http/Controllers/OrderInvoiceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrderInvoice;
+use Illuminate\Http\Request;
+
+class OrderInvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OrderInvoice  $orderInvoice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OrderInvoice $orderInvoice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OrderInvoice  $orderInvoice
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OrderInvoice $orderInvoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OrderInvoice  $orderInvoice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OrderInvoice $orderInvoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OrderInvoice  $orderInvoice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OrderInvoice $orderInvoice)
+    {
+        //
+    }
+}
