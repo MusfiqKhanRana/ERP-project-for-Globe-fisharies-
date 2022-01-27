2db7442db9fce830f53145a6effe56525369a130
Globetech-GTCL <glebetechcompanylimited@gmail.com>
2022-01-27 18:09:57 +0600

    requisition list

--- app/Models/ProductionRequisition.php
+++ app/Models/ProductionRequisition.php
@@ -9,4 +9,13 @@ class ProductionRequisition extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    public function production_supplier()
+    {
+        return $this->belongsTo(ProductionSupplier::class,'production_supplier_id');
+    }
+    public function production_requisition_items()
+    {
+        return $this->belongsToMany(SupplyItem::class,'production_requisition_items','production_requisition_id','supply_item_id')
+        ->withPivot('id','production_requisition_id','rate', 'quantity','amount');
+    }
 }
