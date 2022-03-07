b4e24acea6f97be86d225782f93c550781dbd189
Mahbub2236 <mahbub.globetech@gmail.com>
2022-03-07 18:11:02 +0600

    Block Frozen Update

---
+++ resources/views/backend/production/processing/block_frozen/clean/index.blade.php
@@ -0,0 +1,75 @@
+<div class="tab-pane" id="tab_b">
+    <div class="portlet-title">
+        <div class="caption">
+            <b>Clean List</b>
+        </div>
+        <div class="tools"> </div>
+    </div>
+    <hr>
+    <table class="table table-striped table-bordered table-hover">
+        <thead>
+            <tr>
+                <th>
+                    Invoice No.
+                </th>
+                <th>
+                    Item Name
+                </th>
+                <th>
+                    Grade
+                </th>
+                <th>
+                    Quantity
+                </th>
+                <th style="text-align: center">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    10001
+                </td>
+                <td>
+                    Rui
+                </td>
+                <td>
+                    300-500gm
+                </td>
+                <td>
+                    60kg
+                </td>
+                <td>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#processData_b" class="btn btn-info"><i class="fa fa-refresh" aria-hidden="true"></i> Process Data</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#blocking_b" class="btn btn-success"><i class="fa fa-bar-chart" aria-hidden="true"></i> Bloicking</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#blockCounter_b" class="btn blue"><i class="fa fa-calculator" aria-hidden="true"></i> Block Counter</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#excessVolume_b" class="btn btn-warning"><i class="fa fa-refresh" aria-hidden="true"></i> Excess Volume</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#WastageReturn_b" class="btn btn-danger"><i class="fa fa-refresh" aria-hidden="true"></i> Wastage/Return</button>
+                    
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    200011
+                </td>
+                <td>
+                    Pangash
+                </td>
+                <td>
+                    200-300gm
+                </td>
+                <td>
+                    50kg
+                </td>
+                <td>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#processData_b" class="btn btn-info"><i class="fa fa-refresh" aria-hidden="true"></i> Process Data</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#blocking_b" class="btn btn-success"><i class="fa fa-bar-chart" aria-hidden="true"></i> Bloicking</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#blockCounter_b" class="btn blue"><i class="fa fa-calculator" aria-hidden="true"></i> Block Counter</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#excessVolume_b" class="btn btn-warning"><i class="fa fa-refresh" aria-hidden="true"></i> Excess Volume</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#WastageReturn_b" class="btn btn-danger"><i class="fa fa-refresh" aria-hidden="true"></i> Wastage/Return</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
---
+++ resources/views/backend/production/processing/block_frozen/whole/index.blade.php
@@ -0,0 +1,75 @@
+<div class="tab-pane active" id="tab_a">
+    <div class="portlet-title">
+        <div class="caption">
+            <b>Whole List</b>
+        </div>
+        <div class="tools"> </div>
+    </div>
+    <hr>
+    <table class="table table-striped table-bordered table-hover">
+        <thead>
+            <tr>
+                <th>
+                    Invoice No.
+                </th>
+                <th>
+                    Item Name
+                </th>
+                <th>
+                    Grade
+                </th>
+                <th>
+                    Quantity
+                </th>
+                <th style="text-align: center">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    1001
+                </td>
+                <td>
+                    Rui
+                </td>
+                <td>
+                    300-500gm
+                </td>
+                <td>
+                    60kg
+                </td>
+                <td>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#processData" class="btn btn-info"><i class="fa fa-refresh" aria-hidden="true"></i> Process Data</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#blocking" class="btn btn-success"><i class="fa fa-bar-chart" aria-hidden="true"></i> Bloicking</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#blockCounter" class="btn blue"><i class="fa fa-calculator" aria-hidden="true"></i> Block Counter</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#excessVolume" class="btn btn-warning"><i class="fa fa-refresh" aria-hidden="true"></i> Excess Volume</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#WastageReturn" class="btn btn-danger"><i class="fa fa-refresh" aria-hidden="true"></i> Wastage/Return</button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    1002
+                </td>
+                <td>
+                    Pangash
+                </td>
+                <td>
+                    200-300gm
+                </td>
+                <td>
+                    50kg
+                </td>
+                <td>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#processData" class="btn btn-info"><i class="fa fa-refresh" aria-hidden="true"></i> Process Data</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#blocking" class="btn btn-success"><i class="fa fa-bar-chart" aria-hidden="true"></i> Bloicking</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#blockCounter" class="btn blue"><i class="fa fa-calculator" aria-hidden="true"></i> Block Counter</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#excessVolume" class="btn btn-warning"><i class="fa fa-refresh" aria-hidden="true"></i>  Excess Volume</button>
+                    <button style="margin-bottom:3px" data-toggle="modal" href="#WastageReturn" class="btn btn-danger"><i class="fa fa-refresh" aria-hidden="true"></i>  Wastage/Return</button>
+                    
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
