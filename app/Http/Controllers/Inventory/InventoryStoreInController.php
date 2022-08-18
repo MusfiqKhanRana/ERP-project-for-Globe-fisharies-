a40dad29bf3c17fd53974f53674b941574bab5a8
Globetech-GTCL <glebetechcompanylimited@gmail.com>
2022-08-18 11:13:51 +0600

    inventory Store In

--- app/Http/Controllers/Inventory/InventoryStoreInController.php
+++ app/Http/Controllers/Inventory/InventoryStoreInController.php
@@ -29,34 +29,47 @@ class InventoryStoreInController extends Controller
         return view('backend.production.inventory.cold_storage.bulk_storage',compact('processing_grade','supply_item','pack_size'));
     }
     public function bulk_storage_datapass(Request $request){
-            $processing_grades = ProductionProcessingGrade::with('production_processing_unit')->whereHas('production_processing_unit',function($q)use($request){
-                $q->where('status','Bulk_storage')
-                ->whereBetween('created_at',[Carbon::parse($request->to_date)->format('Y-m-d 00:00:00'),Carbon::parse($request->form_date)->format('Y-m-d 23:59:59')])
-                ->where(function($q) use($request){
-                    if ($request->processing_type == "IQF") {
-                        $q->whereIn('processing_name',['iqf','raw_iqf_shrimp','blanched_iqf_shrimp']);
-                    }elseif ($request->processing_type == "BLOCK") {
-                        $q->whereIn('processing_name',['block_frozen','raw_bf_shrimp','semi_iqf']);
-                    }
-                    elseif ($request->processing_type == "VEGETABLE") {
-                        $q->whereIn('processing_name',['vegetable_iqf','vegetable_block','semi_iqf']);
-                    }
-                    elseif ($request->processing_type == "DRYFISH") {
-                        $q->whereIn('processing_name',['dry_fish']);
-                    }
-                    elseif ($request->processing_type == "SWEET") {
-                        $q->whereIn('processing_name',['Sweet Desert']);
-                    }
-                });
-            })->get()->groupBy('batch_code');
-            $process_data = $this->processBulkStorage($processing_grades);
-            return $process_data;
+        $processing_grades = ProductionProcessingGrade::with('production_processing_unit')->whereHas('production_processing_unit',function($q)use($request){
+            $q->where('status','Bulk_storage')
+            ->whereBetween('created_at',[Carbon::parse($request->to_date)->format('Y-m-d 00:00:00'),Carbon::parse($request->form_date)->format('Y-m-d 23:59:59')])
+            ->where(function($q) use($request){
+                if ($request->processing_type == "IQF") {
+                    $q->whereIn('processing_name',['iqf','raw_iqf_shrimp','blanched_iqf_shrimp']);
+                }elseif ($request->processing_type == "BLOCK") {
+                    $q->whereIn('processing_name',['block_frozen','raw_bf_shrimp','semi_iqf']);
+                }
+                elseif ($request->processing_type == "VEGETABLE") {
+                    $q->whereIn('processing_name',['vegetable_iqf','vegetable_block','semi_iqf']);
+                }
+                elseif ($request->processing_type == "DRYFISH") {
+                    $q->whereIn('processing_name',['dry_fish']);
+                }
+                elseif ($request->processing_type == "SWEET") {
+                    $q->whereIn('processing_name',['Sweet Desert']);
+                }
+            });
+        })->get()->groupBy('batch_code');
+        $process_data = $this->processBulkStorage($processing_grades);
+        return $process_data;
     }
     public function processBulkStorage($data){
         $process_array=[];
-        foreach ($data as $key => $process) {
-            // return $key;
-            array_push($process_array,$key);
+        foreach ($data as $key => $processes) {
+            $process_details = explode("#",$key);
+            $item_name = null;
+            $item_grade = null;
+            $produced = 0;
+            foreach ($processes as $key => $processing_grade) {
+                $produced+=$processing_grade->final_weight;
+            }
+            if (count($process_details)== 6) {
+                $item_name = $process_details[5];
+                $item_grade = $process_details[4];
+            }else{
+                $item_name = $process_details[6];
+                $item_grade = $process_details[5];
+            }
+            array_push($process_array,array('item_name'=>$item_name,'item_grade'=>$item_grade,'production_type'=>$process_details[0],'production_variant'=>$process_details[1],'produced'=>$produced,'reprocessed_in'=>0,'reprocessed_out'=>0,'local'=>0,'damage'=>0));
         }
         return $process_array;
     }
