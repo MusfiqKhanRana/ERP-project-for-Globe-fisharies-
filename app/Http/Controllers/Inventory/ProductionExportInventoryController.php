852dc33b77b13823c8444c26b618e14e62830f40
Mahbub2236 <mahbub.globetech@gmail.com>
2022-08-22 11:44:33 +0600

    Reprocessed

--- app/Http/Controllers/Inventory/ProductionExportInventoryController.php
+++ app/Http/Controllers/Inventory/ProductionExportInventoryController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Inventory;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProcessingBlock;
 use App\Models\ProcessingGrade;
 use App\Models\ProductionExportInventory;
+use App\Models\SupplyItem;
 use Illuminate\Http\Request;
 
 class ProductionExportInventoryController extends Controller
@@ -37,18 +39,30 @@ class ProductionExportInventoryController extends Controller
      */
     public function store(Request $request)
     {
-        //dd($request->toArray);
+        $block = ProcessingBlock::where('id',$request->block_size)->first();
+        //dd($request->toArray());
+        $item = SupplyItem::where('id',$request->item_id)->first();
         $grade = ProcessingGrade::where('id',$request->processing_grade_id)->first();
-        // dd($grade->name);
+        //dd($request->toArray());
+        $batch_code = null;
+        if ($grade) {
+            $batch_code = $request->processing_type.'#'.$request->processing_variant.'#'.$request->item_id.'#'.$request->processing_grade_id.'#'.$grade->name.'#'.$item->name;
+          // dd('iqf batch');
+        }else {
+            $batch_code = $request->processing_name.'#'.$request->processing_variant.'#'.$request->item_id.'#'.$block->id.'#'.$request->block_size.'#'.$item->name;
+        }
         $inventory = new ProductionExportInventory();
         $inventory->storage_name = $request->storage_name;
         $inventory->processing_name = $request->processing_name;
         $inventory->processing_variant = $request->processing_variant;
         $inventory->item_id = $request->item_id;
-        $inventory->batch_code = $request->processing_name.'.'.$request->processing_variant.'.'.$request->item_id.'.'.$request->processing_grade_id.'.'.$grade->name;
+        $inventory->batch_code = $batch_code;
         $inventory->processing_grade_id = $request->processing_grade_id;
         $inventory->export_pack_size_id = $request->export_pack_size_id;
         $inventory->transfer_qty_ctn = $request->transfer_qty_ctn;
+        $inventory->block_quantity = $request->block_quantity;
+        $inventory->fish_grade = $request->fish_grade;
+        $inventory->block_size = $request->block_size;
         $inventory->transfer_qty_kg = $request->transfer_qty_kg;
         $inventory->save();
 
